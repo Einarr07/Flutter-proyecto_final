0c3ad48e62bdc7f2e85f32486720877b8b5b055f
dilan-flores <dilan.flores@epn.edu.ec>
2024-03-01 03:57:56 -0500

    Calcular área de polígono (Terreno)

--- lib/Topografo/TopoPage.dart
+++ lib/Topografo/TopoPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:math' as math;
 
 class UbicacionPage extends StatefulWidget {
   @override
@@ -10,12 +11,16 @@ class UbicacionPage extends StatefulWidget {
 class _UbicacionPageState extends State<UbicacionPage> {
   late bool _locationEnabled;
   late LatLng _userLocation;
+  late Set<Polygon> _polygons;
+  late List<LatLng> _polygonPoints;
 
   @override
   void initState() {
     super.initState();
     _locationEnabled = false;
     _userLocation = LatLng(0, 0);
+    _polygons = Set<Polygon>();
+    _polygonPoints = [];
     _checkLocationPermission();
   }
 
@@ -56,72 +61,122 @@ class _UbicacionPageState extends State<UbicacionPage> {
         // Handle location error
         print('Error getting location: $e');
       }
+    } else {
+      setState(() {
+        _userLocation = LatLng(0, 0);
+      });
     }
   }
 
-  @override
+  void _addPolygonPoint(LatLng point) {
+    setState(() {
+      _polygonPoints.add(point);
+      if (_polygonPoints.length >= 3) {
+        _updatePolygon();
+      }
+    });
+  }
 
+  void _updatePolygon() {
+    setState(() {
+      _polygons.clear();
+      _polygons.add(
+        Polygon(
+          polygonId: PolygonId('polygon'),
+          points: _polygonPoints,
+          strokeWidth: 2,
+          strokeColor: Colors.blue,
+          fillColor: Colors.blue.withOpacity(0.3),
+        ),
+      );
+    });
+  }
+
+  double _calculatePolygonArea() {
+    double area = 0;
+    int j = _polygonPoints.length - 1;
+
+    for (int i = 0; i < _polygonPoints.length; i++) {
+      double xi = _polygonPoints[i].latitude * math.pi / 180;
+      double yi = _polygonPoints[i].longitude * math.pi / 180;
+      double xj = _polygonPoints[j].latitude * math.pi / 180;
+      double yj = _polygonPoints[j].longitude * math.pi / 180;
+
+      area += (xj + xi) * (yj - yi);
+      j = i;
+    }
+
+    return (area.abs() / 2) * 6378137 * 6378137; // Radio medio de la Tierra al cuadrado
+  }
+
+  @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: Text('Topografos'),
+        title: Text('Plataforma de Ubicación y Mapeo'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('¿Quieres activar la ubicación?'),
-            ElevatedButton(
-              onPressed: () async {
-                bool enableLocation = await showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Activar Ubicación'),
-                      content: Text('¿Quieres activar la ubicación?'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(true);
-                          },
-                          child: Text('Sí'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(false);
-                          },
-                          child: Text('No'),
-                        ),
-                      ],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(0, 0), // Ubicación inicial (ecuador)
+                zoom: 2,
+              ),
+              markers: _locationEnabled
+                  ? {
+                Marker(
+                  markerId: MarkerId('userLocation'),
+                  position: _userLocation,
+                  infoWindow: InfoWindow(title: 'Tu Ubicación'),
+                ),
+              }
+                  : {},
+              polygons: _polygons,
+              onTap: _addPolygonPoint,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _polygons.clear();
+                      _polygonPoints.clear();
+                    });
+                  },
+                  child: Text('Limpiar'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    double area = _calculatePolygonArea();
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Área del terreno'),
+                          content: Text('El área del terreno es: $area'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Aceptar'),
+                            ),
+                          ],
+                        );
+                      },
                     );
                   },
-                );
-
-                if (enableLocation) {
-                  _checkLocationPermission();
-                }
-              },
-              child: Text('Activar Ubicación'),
-            ),
-            if (_locationEnabled)
-              Expanded(
-                child: GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                    target: _userLocation,
-                    zoom: 15,
-                  ),
-                  markers: {
-                    Marker(
-                      markerId: MarkerId('userLocation'),
-                      position: _userLocation,
-                      infoWindow: InfoWindow(title: 'Tu Ubicación'),
-                    ),
-                  },
+                  child: Text('Calcular Área'),
                 ),
-              ),
-          ],
-        ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
