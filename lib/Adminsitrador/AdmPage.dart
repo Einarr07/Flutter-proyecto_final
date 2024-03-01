7af1dfa48865782b430ce5adf2bc780516af33e0
erick200011 <75103508+erick200011@users.noreply.github.com>
2024-03-01 08:22:38 -0500

    visualización desde administrador

--- lib/Adminsitrador/AdmPage.dart
+++ lib/Adminsitrador/AdmPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:zona/Topografo/TopoPage.dart'; // Asegúrate de importar TopoPageWidget desde la ruta correcta
 
 class PaginaAdmin extends StatefulWidget {
   @override
@@ -72,15 +73,18 @@ class _PaginaAdminState extends State<PaginaAdmin> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Agregar Usuario', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 10), // Agrega un espacio entre el texto y los campos de entrada
             TextField(
               controller: _emailController,
               decoration: InputDecoration(labelText: 'Correo'),
             ),
+            SizedBox(height: 10), // Agrega un espacio entre los campos de entrada
             TextField(
               controller: _passwordController,
               decoration: InputDecoration(labelText: 'Contraseña'),
               obscureText: true,
             ),
+            SizedBox(height: 10), // Agrega un espacio entre los campos de entrada y el botón
             ElevatedButton(
               onPressed: () {
                 signUpWithEmailAndPassword(_emailController.text, _passwordController.text);
@@ -91,45 +95,52 @@ class _PaginaAdminState extends State<PaginaAdmin> {
             ),
             SizedBox(height: 20),
             Text('Activar/Inavilitar o eliminar un usuario', style: TextStyle(fontSize: 20)),
-            StreamBuilder<QuerySnapshot>(
-  stream: FirebaseFirestore.instance.collection('usuarios').snapshots(),
-  builder: (context, snapshot) {
-    if (snapshot.hasData) {
-      return Column(
-        children: snapshot.data!.docs.map((doc) {
-          final email = doc.get('email');
-          final activo = doc.get('activo');
+            SizedBox(height: 10), // Agrega un espacio entre el texto y la lista de usuarios
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('usuarios').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView(
+                      children: snapshot.data!.docs.map((doc) {
+                        final email = doc.get('email');
+                        final activo = doc.get('activo');
 
-          if (email != null) {
-            return ListTile(
-              title: Text(email),
-              subtitle: Text(activo ? 'Activo' : 'Inactivo'),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.toggle_on),
-                    onPressed: () => toggleUserActiveStatus(doc.id, !activo),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => deleteUser(doc.id),
-                  ),
-                ],
+                        if (email != null) {
+                          return ListTile(
+                            title: Text(email),
+                            subtitle: Text(activo ? 'Activo' : 'Inactivo'),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.toggle_on),
+                                  onPressed: () => toggleUserActiveStatus(doc.id, !activo),
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () => deleteUser(doc.id),
+                                ),
+                              ],
+                            ),
+                          );
+                        } else {
+                          return SizedBox(); // O cualquier otro widget para manejar el caso sin email
+                        }
+                      }).toList(),
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
               ),
-            );
-          } else {
-            return SizedBox(); // O cualquier otro widget para manejar el caso sin email
-          }
-        }).toList(),
-      );
-    } else {
-      return CircularProgressIndicator();
-    }
-  },
-),
-
-
+            ),
+            SizedBox(height: 20),
+            Text('Visualización de Posición', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 10), // Agrega un espacio entre el texto y el mapa
+            Expanded(
+              child: TopoPageWidget(), // Mueve TopoPageWidget dentro de un Expanded para que ocupe todo el espacio disponible
+            ),
           ],
         ),
       ),
